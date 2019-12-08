43155273b9a67f5815b90c1a057f0c384075086c
Taras <landgradus@gmail.com>
2019-12-08 23:12:37 +0300

    Add Telegram bot API

--- tbot.go
+++ tbot.go
@@ -3,10 +3,16 @@ package main
 import (
 	"fmt"
 	"github.com/bxcodec/faker"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 	"github.com/satori/go.uuid"
 	"github.com/taras-by/tbot/store"
+	"io/ioutil"
 	"log"
 	"time"
+)
+
+const (
+	telegramTokenFile = "telegram_token"
 )
 
 func main() {
@@ -15,6 +21,18 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	token, err := ioutil.ReadFile(telegramTokenFile)
+	if err != nil {
+		log.Panic(err.Error())
+	}
+
+	bot, err := tgbotapi.NewBotAPI(string(token))
+	if err != nil {
+		log.Panic(err.Error())
+	}
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
 
 	storage.Create(store.Participant{
 		User: store.User{
